fd25f33df6ceb0e47eed71b82a9213c35872d8fb
yutoc11 <37706720-yutoc11@users.noreply.replit.com>
2024-11-19 13:17:06 +0000

    Checkpoint before revert - Add compatibility matching between different vegetable/fruit types

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ from app.config.development import DevelopmentConfig
 app = create_app(DevelopmentConfig)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=5000
+    )
